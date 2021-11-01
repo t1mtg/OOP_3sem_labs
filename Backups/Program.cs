f69d02426854904a57a4a15648e2ae8ce9afb291
t1mtg <timagurman@gmail.com>
2021-11-01 23:24:36 +0300

    fix: unused namespace

--- Backups/Program.cs
+++ Backups/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-using NUnit.Framework;
-
+﻿using System.Collections.Generic;
 namespace Backups
 {
     internal class Program
